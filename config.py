4139661d4c861c835ba3169488ef27442750541f
DR-Dev-Jack <114923262+DR-Dev-Jack@users.noreply.github.com>
2024-02-06 15:53:15 +0100

    collision
    added a simple collision function

--- config.py
+++ config.py
@@ -1,17 +1,24 @@
-# you or ai?
-ai_play = False  # not working yet
+# you or AI?
+ai_play = False  # AI not working yet
 
-# game config
+# game config:
 acceleration_speed = 0.03  # speed with witch you accelerate. Standard 0.01
 brake_speed = 0.4  # speed with witch you brake. Standard 0.2
 steering_speed = 0.15  # speed with witch you steer. Standard 0.1
 stable_civilian_car_speed = 5
-speedometer = True  # if set to false dashboard disepears. Standard True
+speedometer = True  # if set to false dashboard disappears. Standard True
 crazy_speedometer = True  # if set to false speedometer wont go "ghelicopter ghelicopter"
-max_speed = 60  # not working yet
+max_speed = 120  # set the top speed of the car
 
-# ai config
+collision = True
+
+# ai config:
+rays = 16  # not working yet
+ai_can_see_horizon = True  # not working yet
 neurons = 10  # not working yet
 velocity_bonus = 0  # not working yet
 crash_punishment = 0  # not working yet
 
+# bug testing:
+hitbox_visuals = True  # shows the hit box for collision bug testing
+raycasting_visuals = False  # shows ray casting for bug testing and fun
--- main.py
+++ main.py
@@ -14,8 +14,10 @@ oversized_dashboard = pg.image.load('photos/dashboard.png')
 oversized_dashboard_pointer = pg.image.load('photos/dashboard_metermeter.png')
 
 Car = pg.transform.scale(Oversized_car, (600, 600))
+
 civilian_car = pg.transform.scale(Oversized_civilian_car, (175, 175))
 civilian_car2 = pg.transform.scale(Oversized_civilian_car2, (175, 175))
+
 dashboard = pg.transform.scale(oversized_dashboard, (300, 300))
 dashboard_pointer = pg.transform.scale(oversized_dashboard_pointer, (300, 300))
 
@@ -28,6 +30,7 @@ surrounding = 0
 civilian_cars = []
 angle = -180
 roads = [301, 401, 501, 601]
+crash_counter = 0
 
 
 # Pygame update function
@@ -43,16 +46,20 @@ while running:
 
     keys = pg.key.get_pressed()
 
-    if keys[pg.K_w] or keys[pg.K_UP]:
-        velocity += acceleration_speed
-    if keys[pg.K_s] or keys[pg.K_DOWN]:
-        if velocity >= 0.2:
-            velocity -= brake_speed
-    if keys[pg.K_a] and x >= 105 or keys[pg.K_LEFT] and x >= 105:
-        x -= velocity * steering_speed
+    if keys[pg.K_ESCAPE]:
+        exit("Program stopped, cause: User pressed escape.")
 
-    if keys[pg.K_d] and x <= 405 or keys[pg.K_RIGHT] and x <= 405:
-        x += velocity * steering_speed
+    if not ai_play:
+        if keys[pg.K_w] or keys[pg.K_UP]:
+            velocity += acceleration_speed
+        if keys[pg.K_s] or keys[pg.K_DOWN]:
+            if velocity >= 0.2:
+                velocity -= brake_speed
+        if keys[pg.K_a] and x >= 105 or keys[pg.K_LEFT] and x >= 105:
+            x -= velocity * steering_speed
+
+        if keys[pg.K_d] and x <= 405 or keys[pg.K_RIGHT] and x <= 405:
+            x += velocity * steering_speed
 
     surrounding += velocity
     travel_lenght += velocity
@@ -60,37 +67,54 @@ while running:
     if surrounding >= 800:
         surrounding = 0
 
-    display.blit(Road, (0, surrounding - 800))
-    display.blit(Road, (0, surrounding))
-
     if len(civilian_cars) < 3:
         color = random.randint(1, 2)
         road = random.choice(roads)
         speed = random.uniform(0.20, 0.85)
         civilian_cars.append({"color": color, "road": road, "y": -165, "speed": speed})
 
+    # drawing on screen
+    display.blit(Road, (0, surrounding - 800))
+    display.blit(Road, (0, surrounding))
+
+    display.blit(Car, (x - 240, 200))  # huh? why  -240?
+    if hitbox_visuals:
+        Car_hitbox = pg.Rect(x, 458, 90, 120)
+        pg.draw.rect(display, (255, 0, 0), Car_hitbox, 1)
+
     for car in civilian_cars:
         color = car["color"]
         road = car["road"]
         y = car["y"]
         speed = car["speed"]
+        civilian_car_hitbox = pg.Rect(road - 196, y + 12, 89, 154)
+        Car_hitbox = pg.Rect(x, 458, 90, 120)
         if color == 1:
             display.blit(civilian_car, (road - 240, y))
+            if hitbox_visuals:
+                pg.draw.rect(display, (255, 0, 0), civilian_car_hitbox, 1)
         else:
             display.blit(civilian_car2, (road - 240, y))
+            if hitbox_visuals:
+                pg.draw.rect(display, (255, 0, 0), civilian_car_hitbox, 1)
+        if collision:
+            if civilian_car_hitbox.colliderect(Car_hitbox):
+                crash_counter += 1
+
         car["y"] += speed * (velocity - stable_civilian_car_speed)
+
         if y >= 965:
             civilian_cars.remove(car)
 
-    display.blit(Car, (x - 240, 200))  # huh? why  -240?
-
     if speedometer:
         speed_in_degrees = -velocity * 4.5 - 180
-        if velocity < 61:
+        if velocity >= max_speed:
+            velocity = max_speed
+        elif velocity < 61:
             angle = speed_in_degrees
-        elif velocity >= 61 and velocity <= 100:
+        elif 61 <= velocity <= 100:
             angle = -random.randint(450, 465)
-        else:
+        elif velocity >= 101:
             if crazy_speedometer:
                 angle -= 25
             else:
@@ -101,4 +125,5 @@ while running:
 
         display.blit(dashboard, (350, 550))
         display.blit(dashboard_pointer_rotated, rect.topleft)
+        print("crashes:", crash_counter)
     update()
