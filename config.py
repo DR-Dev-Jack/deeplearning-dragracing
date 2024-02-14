d79401f4b3612dbb448dda68c44e4d1f5334617c
DR-Dev-Jack <114923262+DR-Dev-Jack@users.noreply.github.com>
2024-02-14 15:34:53 +0100

    added DIY raycasting
    i added a DIY solution that looks like raycasting, but easier

--- config.py
+++ config.py
@@ -2,23 +2,24 @@
 ai_play = False  # AI not working yet
 
 # game config:
-acceleration_speed = 0.03  # speed with witch you accelerate. Standard 0.01
-brake_speed = 0.4  # speed with witch you brake. Standard 0.2
-steering_speed = 0.15  # speed with witch you steer. Standard 0.1
+acceleration_speed = 0.03  # speed with witch you accelerate. Standard 0.03
+brake_speed = 0.4  # speed with witch you brake. Standard 0.4
+steering_speed = 0.15  # speed with witch you steer. Standard 0.15
 stable_civilian_car_speed = 5
 speedometer = True  # if set to false dashboard disappears. Standard True
 crazy_speedometer = True  # if set to false speedometer wont go "ghelicopter ghelicopter"
 max_speed = 120  # set the top speed of the car
 
 collision = True
+raycasting = True
+crash_is_game_over = True
 
 # ai config:
-rays = 16  # not working yet
-ai_can_see_horizon = True  # not working yet
+ai_can_see_x_cords = True  # not working yet
 neurons = 10  # not working yet
 velocity_bonus = 0  # not working yet
 crash_punishment = 0  # not working yet
 
 # bug testing:
 hitbox_visuals = True  # shows the hit box for collision bug testing
-raycasting_visuals = False  # shows ray casting for bug testing and fun
+raycasting_visuals = True  # shows ray casting for bug testing and fun
--- main.py
+++ main.py
@@ -31,12 +31,61 @@ civilian_cars = []
 angle = -180
 roads = [301, 401, 501, 601]
 crash_counter = 0
+tick = 0
 
 
 # Pygame update function
 def update():
     pg.display.update()
     display.fill((0, 0, 0))
+
+
+def ray(x, y, max_ray_length, oriantation, list):
+    distance = 0
+    original_x = x
+    original_y = y
+    break_e = False
+    for ray_length in range(max_ray_length):
+        if oriantation == "Up":
+            y -= 1  # Move up
+        elif oriantation == "Down":
+            y += 1  # Move down
+        elif oriantation == "Left":
+            x += 1  # Move left
+        elif oriantation == "Right":
+            x -= 1  # Move right
+        elif oriantation == "Up-Left":
+            x += 1  # Move left
+            y -= 1  # Move up
+        elif oriantation == "Up-Right":
+            x -= 1  # Move right
+            y -= 1  # Move up
+        elif oriantation == "Down-Left":
+            x += 1  # Move left
+            y += 1  # Move down
+        elif oriantation == "Down-Right":
+            x -= 1  # Move right
+            y += 1  # Move down
+        else:
+            exit("Program stopped, cause: Invalid ray orientation")
+        distance += 1
+
+        shortest_ray = max_ray_length
+        for item in list:
+            temp_y = item["y"]
+            temp_road = item["road"]
+            temp_car_hitbox = pg.Rect(temp_road - 196, temp_y + 12, 89, 154)
+            ray_prediction = pg.Rect(x, y, 1, 1)
+
+            if temp_car_hitbox.colliderect(ray_prediction):
+                if distance <= shortest_ray:
+                    shortest_ray = distance
+                    break_e = True
+        if break_e:
+            break
+    if raycasting_visuals:
+        pg.draw.line(display, (0, 255, 0), (original_x, original_y), (x, y))
+    return shortest_ray
 
 
 while running:
@@ -47,7 +96,7 @@ while running:
     keys = pg.key.get_pressed()
 
     if keys[pg.K_ESCAPE]:
-        exit("Program stopped, cause: User pressed escape.")
+        exit("Program stopped, cause: User pressed escape!")
 
     if not ai_play:
         if keys[pg.K_w] or keys[pg.K_UP]:
@@ -106,6 +155,15 @@ while running:
         if y >= 965:
             civilian_cars.remove(car)
 
+    ray_forward = ray(x + 45, 458, 100, "Up", civilian_cars)
+    ray_forward_left = ray(x + 45, 458, 100, "Up-Left", civilian_cars)
+    ray_forward_right = ray(x + 45, 458, 100, "Up-Right", civilian_cars)
+    ray_right = ray(x, 458 + 30, 100, "Right", civilian_cars)
+    ray_right_down = ray(x, 458 + 30, 100, "Down-Right", civilian_cars)
+    ray_left = ray(x + 90, 458 + 30, 100, "Left", civilian_cars)
+    ray_left_down = ray(x + 90, 458 + 30, 100, "Down-Left", civilian_cars)
+    ray_down = ray(x + 45, 458 + 120, 100, "Down", civilian_cars)
+
     if speedometer:
         speed_in_degrees = -velocity * 4.5 - 180
         if velocity >= max_speed:
@@ -125,5 +183,7 @@ while running:
 
         display.blit(dashboard, (350, 550))
         display.blit(dashboard_pointer_rotated, rect.topleft)
-        print("crashes:", crash_counter)
+        if crash_counter >= 1 and crash_is_game_over:
+            print("final distance:", round(travel_lenght))
+            exit("Program stopped, cause: you crashed!")
     update()
